4ee5b4f3560c5b4c098927d6394b5a054af54011
Josh Steward <stewardjosh10@gmail.com>
2021-02-15 18:45:35 +1100

    files deleted

--- testss/Engineer.test.js
+++
@@ -1,7 +0,0 @@
-const Engineer = require ('../lib/Engineer');
-
-test("getRole() should return \"Engineer\"", () => {
-    const testValue = "Engineer";
-    const e = new Engineer("Josh", 1, "test@test.com");
-    expect(e.getRole()).toBe(testValue);
-});
